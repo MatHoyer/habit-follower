9758467b91c4d7ab3c17beeee9b119ee7bd5ea11
MatHoyer <mathieuhoyer@gmail.com>
2024-06-25 14:49:53 +0200

    dont center if there is a lot of elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { LoginButton, LogoutButton } from '@/components/AuthButton';
 import { TodoTable } from '@/components/TodoTable';
 import { auth } from '@/lib/auth';
 import prisma from '@/lib/prisma';
+import { cn } from '@/lib/utils';
 
 const Home = async () => {
   const session = await auth();
@@ -15,7 +16,7 @@ const Home = async () => {
   });
 
   return (
-    <div className="flex flex-col justify-center items-center gap-3 h-screen">
+    <div className={cn('flex flex-col items-center gap-3 h-screen', !session?.user && 'justify-center')}>
       {session?.user ? (
         <div>
           <TodoTable todos={todos} />
