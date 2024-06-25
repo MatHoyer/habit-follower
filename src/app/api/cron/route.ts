c0ff23ff7e1f73f689528d0475e213361ff9024e
MatHoyer <mathieuhoyer@gmail.com>
2024-06-25 23:27:40 +0200

    protect api route

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,13 @@
 import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
+  const host = request.headers.get('host');
+
+  if (host && (!host.includes('localhost') || !host.startsWith('127.0.0.1')))
+    return NextResponse.json({ status: 'un authorized' });
+
   const todos = await prisma.todo.findMany();
 
   todos.map(async (todo) => {
