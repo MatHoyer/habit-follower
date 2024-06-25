84cd00abd45e64ca495e11ab1c13c6e97c6bbad7
MatHoyer <mathieuhoyer@gmail.com>
2024-06-25 23:16:04 +0200

    try patch

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export const GET = async () => {
   const todos = await prisma.todo.findMany();
 
